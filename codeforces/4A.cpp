d869d44e6a5976305c2d0540e305d2a606348a11
newbierash <devrashad29@outlook.com>
2024-08-30 15:36:27 +0400

    4A added

---
+++ codeforces/4A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define $rash$ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define int ll
+#define nl endl
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define vc vector<char>
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define eb emplace_back
+#define f first
+#define s second
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    if (n%2 == 0 && n!=2) cout << "YES";
+    else cout << "NO";
+}
+
+signed main()
+{
+    $rash$
+    solve();
+    return 0;
+}

5852066bb285f2489e88773745b4253beb24963b
newbierash <devrashad29@outlook.com>
2024-08-31 01:32:55 +0400

    solved 50A

---
+++ codeforces/50A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define $rash$ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define int ll
+#define nl endl
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define vc vector<char>
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define eb emplace_back
+#define f first
+#define s second
+using namespace std;
+
+int n,m;
+
+void solve(){
+    cin >> n >> m;
+    cout << n*m/2;
+}
+
+signed main()
+{
+    $rash$
+    solve();
+    return 0;
+}

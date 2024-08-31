2c88d52b62e5c4428a32af13c57451645182ad73
newbierash <devrashad29@outlook.com>
2024-08-31 16:08:13 +0400

    263A and 112A solved

--- codeforces/112A.cpp
+++ codeforces/112A.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 #define $rash$ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define ll long long
@@ -15,20 +14,24 @@
 #define s second
 using namespace std;
 
-int arr[5][5];
-int x,y;
+string s1,s2;
+int res=0;
 
 void solve(){
-    for (int i = 0; i<5; i++){
-        for (int j = 0; j<5; j++){
-            cin >> arr[i][j];
-            if (arr[i][j] == 1){
-                x = i+1;
-                y = j+1;
-            }
+    cin >> s1 >> s2;
+    for (int i = 0; i<s1.size(); i++){
+        s1[i] = toupper(s1[i]);
+        s2[i] = toupper(s2[i]);
+        if (s1[i] > s2[i]){
+            res = 1;
+            break;
+        }
+        else if(s1[i] < s2[i]){
+            res = -1;
+            break;
         }
     }
-    cout << abs(x-3) + abs(y-3);
+    cout << res;
 }
 
 signed main()

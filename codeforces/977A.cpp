6fe3f9e172e078be49e21ed3fda49db5b4cb9a56
newbierash <devrashad29@outlook.com>
2024-09-03 10:47:58 +0400

    977A solved

---
+++ codeforces/977A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define $rash$ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define int ll
+#define nl endl
+#define vi vector<int>
+#define vc vector<char>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define eb emplace_back
+#define f first
+#define s second
+using namespace std;
+
+int n,k;
+
+void solve(){
+    cin >> n >> k;
+    for (int i = 0; i<k;i++){
+        if (n%10 == 0) n/=10;
+        else --n;
+    }
+    cout << n;
+}
+
+signed main()
+{
+    $rash$
+    solve();
+    return 0;
+}
